5de2af507a6d2828f2a3c2c0eb5cf496ff0ae899
bilalmas <20100034@lums.edu.pk>
2019-10-17 23:03:16 +0500

    RecyclerView implemented in People and Chat Fragment

--- app/src/main/java/com/bilalmas/eliteclub/FragmentChat.java
+++ app/src/main/java/com/bilalmas/eliteclub/FragmentChat.java
@@ -1,18 +1,111 @@
 package com.bilalmas.eliteclub;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class FragmentChat extends Fragment {
+    private RecyclerView FindFriendsRecyclerList;
+    private DatabaseReference UsersRef;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chat,null);
+        View view= inflater.inflate(R.layout.fragment_chat,null);
+
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+
+        FindFriendsRecyclerList = (RecyclerView)view.findViewById(R.id.find_friends_recycler_list);
+        FindFriendsRecyclerList.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
+        return view;
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Users> options =
+                new FirebaseRecyclerOptions.Builder<Users>()
+                        .setQuery(UsersRef, Users.class)
+                        .build();
+
+        FirebaseRecyclerAdapter<Users, FragmentFeed.FindFriendViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Users, FragmentFeed.FindFriendViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull FragmentFeed.FindFriendViewHolder holder, final int position, @NonNull Users model)
+                    {
+
+                        holder.userName.setText(model.getUsername());
+                       // holder.userStatus.setText(model.getBio());
+                        //holder.interests.setText(model.getInterests());
+                        Picasso.with(getContext()).load(model.getImage()).placeholder(R.drawable.profile_image).into(holder.profileImage);
+
+
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view)
+                            {
+                                String visit_user_id = getRef(position).getKey();
+
+                                Intent profileIntent = new Intent(getActivity(), ChatActivity.class);
+                                profileIntent.putExtra("visit_user_id", visit_user_id);
+                                startActivity(profileIntent);
+                            }
+                        });
+                    }
+
+                    @NonNull
+                    @Override
+                    public FragmentFeed.FindFriendViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i)
+                    {
+                        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.users_chatlayout, viewGroup, false);
+                        FragmentFeed.FindFriendViewHolder viewHolder = new FragmentFeed.FindFriendViewHolder(view);
+                        return viewHolder;
+                    }
+                };
+
+        FindFriendsRecyclerList.setAdapter(adapter);
+
+        adapter.startListening();
+    }
+
+
+
+    public static class FindFriendViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView userName, userStatus,interests;
+        CircleImageView profileImage;
+
+
+        public FindFriendViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+
+            userName = itemView.findViewById(R.id.username);
+            userStatus = itemView.findViewById(R.id.bio);
+            interests =  itemView.findViewById(R.id.interests);
+            profileImage = itemView.findViewById(R.id.users_profile_image);
+        }
     }
 }
--- app/src/main/java/com/bilalmas/eliteclub/FragmentFeed.java
+++ app/src/main/java/com/bilalmas/eliteclub/FragmentFeed.java
@@ -1,18 +1,112 @@
 package com.bilalmas.eliteclub;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class FragmentFeed extends Fragment {
+
+    private RecyclerView FindFriendsRecyclerList;
+    private DatabaseReference UsersRef;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragmentfeed,null);
+        View view= inflater.inflate(R.layout.fragmentfeed,null);
+
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+
+        FindFriendsRecyclerList = (RecyclerView)view.findViewById(R.id.find_friends_recycler_list);
+        FindFriendsRecyclerList.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
+        return view;
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Users> options =
+                new FirebaseRecyclerOptions.Builder<Users>()
+                        .setQuery(UsersRef, Users.class)
+                        .build();
+
+        FirebaseRecyclerAdapter<Users, FindFriendViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Users, FindFriendViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull FindFriendViewHolder holder, final int position, @NonNull Users model)
+                    {
+                        
+                        holder.userName.setText(model.getUsername());
+                        holder.userStatus.setText(model.getBio());
+                        holder.interests.setText(model.getInterests());
+                        Picasso.with(getContext()).load(model.getImage()).placeholder(R.drawable.profile_image).into(holder.profileImage);
+
+
+                        /*holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view)
+                            {
+                                String visit_user_id = getRef(position).getKey();
+
+                                Intent profileIntent = new Intent(FindFriendsActivity.this, ProfileActivity.class);
+                                profileIntent.putExtra("visit_user_id", visit_user_id);
+                                startActivity(profileIntent);
+                            }
+                        });*/
+                    }
+
+                    @NonNull
+                    @Override
+                    public FindFriendViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i)
+                    {
+                        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.users_layout, viewGroup, false);
+                        FindFriendViewHolder viewHolder = new FindFriendViewHolder(view);
+                        return viewHolder;
+                    }
+                };
+
+        FindFriendsRecyclerList.setAdapter(adapter);
+
+        adapter.startListening();
+    }
+
+
+
+    public static class FindFriendViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView userName, userStatus,interests;
+        CircleImageView profileImage;
+
+
+        public FindFriendViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+
+            userName = itemView.findViewById(R.id.username);
+            userStatus = itemView.findViewById(R.id.bio);
+            interests =  itemView.findViewById(R.id.interests);
+            profileImage = itemView.findViewById(R.id.users_profile_image);
+        }
     }
 }
